c3d7d1f6037f01642ce68b528d84375b18528722
debajitdatta2k <34594080+debajitdatta2k@users.noreply.github.com>
2018-03-12 22:49:01 +0530

    Initial commit

---
+++ app/src/main/java/com/debajit/volley/moviedirectory/Util/Prefs.java
@@ -0,0 +1,26 @@
+package com.debajit.volley.moviedirectory.Util;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by debaj on 3/12/2018.
+ */
+
+public class Prefs {
+
+    SharedPreferences sharedPreferences;
+
+    public Prefs(Activity activity){
+        sharedPreferences = activity.getPreferences(activity.MODE_PRIVATE);
+    }
+
+    public void setSearch(String search){
+        sharedPreferences.edit().putString("search",search).commit();
+    }
+
+    public String getSearch(){
+        return sharedPreferences.getString("Search","Batman");
+    }
+
+}
