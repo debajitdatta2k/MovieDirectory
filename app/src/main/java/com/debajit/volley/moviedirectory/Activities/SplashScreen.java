3aa3efe3cc6735c500886a640cbb3944de2096be
debajitdatta2k <34594080+debajitdatta2k@users.noreply.github.com>
2018-03-31 19:24:07 +0530

    bug fixes 2.5

---
+++ app/src/main/java/com/debajit/volley/moviedirectory/Activities/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.debajit.volley.moviedirectory.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.debajit.volley.moviedirectory.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int splashTimeOut=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },splashTimeOut);
+
+    }
+}
